9f705698729fecf33aca35671852e8ac27012061
ilhom <inspirationjon@abutech.uz>
2021-11-30 21:53:45 +0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+#This page covers MC progtam functions and whole program devided into three functions
+
+
+#MC chart program questions assigned in tuple data type and named as mc_questions
+mc_questions = (
+                'Did you upload it on time?',
+                'Was it within 24 hours?',
+                'Is there a valid reason?',
+                'Did you submitted within 5 days?',
+                'Was it accepted?')
+
+#A list data type was used to answer the questions and assigned to mc_answers
+mc_answers = ['You will get Full Mark',
+              'Minus 10 marks from overall mark but not below 40',
+              'Anfortunately, you will get Mark = 0',
+              'Deferral reassessment',
+              'Late Submission',  'MC']
